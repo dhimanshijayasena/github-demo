cc2e6ae2fac2bc93d9047c7ed7249a51b7cb5615
dhimanshijayasena <81155966+dhimanshijayasena@users.noreply.github.com>
2021-03-22 15:48:25 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,22 @@
+# Add implementation
+def add(x,y):
+    return x+y
+    
+# Substract implementation    
+def subtract(x,y):
+    return x-y
+    
+#Multiply implementation
+def multiply(x,y):
+    return x*y
+    
+# Divide implementation
+def divide(x,y):
+    if y==0:
+        return DIVIDE_BY_0_ERROR
+    error:
+        return x/y
+
+#Square implementation        
+def square(x)
+    pass
